297606b1092ee367b90eca579367ef26f855045b
Ryan Hu <ryanhuuu@gmail.com>
2018-03-20 21:51:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(speakers)
   result = []
   for i in 1..7 do |i|
-    
+    result.push(  "Hello, #{speakers[i-1]! You'll be assigned to room _____!"  )
   end
   return result
 end
